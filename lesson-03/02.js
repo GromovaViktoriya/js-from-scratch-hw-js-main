8daacefea5665cc084b1ef53f3f768daf1e6a9be
Viktoriya Gromova <rockingirl@mail.ru>
2025-07-23 22:16:53 +0500

    added hw lesson 03 part 02

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,9 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(num) {
+    let evenNumber = num % 2 === 0;
+    return evenNumber;
+}
+
+console.log(isEven(7))
